985220a8b983f8195f47cde8be7182bd517fe8dd
JadeVasun <jadevasun@gmail.com>
2017-09-08 19:39:39 -0700

    completed GET should return content of index.html

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,6 +14,15 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  fs.readFile(archive.paths.siteAssets + asset, 'utf-8', function (err, results) {
+    if (err) {
+      callback(err);
+    } else {
+      //console.log(results);
+      res.writeHead(200, exports.headers);
+      res.end(results);
+    }
+  });
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
 // require more modules/folders here!
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET') {
+    
+    var url;
+    if (req.url === '/') {
+      url = '/index.html';
+      
+    }
+  }
+  helpers.serveAssets(res, url);
+  // res.end(archive.paths.list);
 };
